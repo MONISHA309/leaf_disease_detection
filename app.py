28211d659cb1f4ab96d27a23d4c7a94b261e7f3b
MONISHA309 <106957950+MONISHA309@users.noreply.github.com>
2024-11-17 14:10:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
-import cv2
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 from PIL import Image
 
 # Load your trained model (ensure the path is correct)
